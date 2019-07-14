e350cd78a37db070223e71a2a4e170c061b41737
Steven Diamante <steven@pivotal.io>
2019-07-13 22:48:56 -0500

    add git hook to run tests before commit

--- README.md
+++ README.md
@@ -32,3 +32,6 @@ docker start postgres-dev
 
 
 docker run -d -v jenkins_home:/var/jenkins_home --name jenkins -p 8080:8080 -p 50000:50000 jenkins/jenkins:alpine
+
+## GIT HOOKS
+There is pre-commit that will run all tests before commiting.
--- src/test/java/com/diamante/orderingsystem/config/JwtTokenProviderTest.java
+++ src/test/java/com/diamante/orderingsystem/config/JwtTokenProviderTest.java
@@ -4,6 +4,10 @@ import org.junit.Test;
 
 public class JwtTokenProviderTest {
 
+    // io.jsonwebtoken.JwtException: Expired or invalid JWT token
+    //	at com.diamante.orderingsystem.config.JwtTokenProvider.validateToken(JwtTokenProvider.java:69) ~[classes/:na]
+    //	at com.diamante.orderingsystem.config.JwtTokenFilter.doFilter(JwtTokenFilter.java:24) ~[classes/:na]
+
     @Test
     public void createToken() {
     }
