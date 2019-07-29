440ce8e832df62113b9b99eddb5bd6d3520e78ff
Steven Diamante <steven@pivotal.io>
2019-07-28 20:14:33 -0500

    Add admin user to Heroku using config vars to keep username/password secret. [Closes #18]

--- src/main/java/com/diamante/orderingsystem/bootstrap/AuthClientLoader.java
+++ src/main/java/com/diamante/orderingsystem/bootstrap/AuthClientLoader.java
@@ -4,6 +4,7 @@ import com.diamante.orderingsystem.entity.User;
 import com.diamante.orderingsystem.repository.security.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -12,7 +13,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 
-@Profile("dev")
+@Profile({"dev", "prod"})
 @RequiredArgsConstructor
 @Slf4j
 @Component
@@ -20,16 +21,26 @@ public class AuthClientLoader implements CommandLineRunner {
 
     private final UserRepository userRepository;
 
+    @Value("${jwt.username}")
+    private String username;
+
+    @Value("${jwt.password}")
+    private String password;
+
+    private User adminUser;
+
     private PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
-    private User adminUser = User.builder()
-            .username("admin")
-            .password(passwordEncoder.encode("password"))
-            .roles(Collections.singletonList("ROLE_ADMIN"))
-            .build();
 
     @Override
     public void run(String... args) throws Exception {
+        adminUser = User.builder()
+                .username(username)
+                .password(passwordEncoder.encode(password))
+                .roles(Collections.singletonList("ROLE_ADMIN"))
+                .build();
+
+
         userRepository.deleteAll();
         userRepository.save(adminUser);
     }
